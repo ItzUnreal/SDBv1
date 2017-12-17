16156b15202740096baa6b500acbaba0b2b70c11
ItzUnreal <34492171+ItzUnreal@users.noreply.github.com>
2017-12-17 14:22:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ bot.on('ready', () => {
 bot.on('message', (message) => {
     if(dead == false) {
 if(message.channel.name == spam && stop == false) {
+    console.log("Sayori is now spamming in " + message.guild.name);
     message.channel.send("Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head! Get out of my head!");
 }
 if(message.content == prefix+"stop") {
@@ -27,6 +28,7 @@ if(message.content == prefix+"help") {
     message.channel.send("*All Commands: (Prefix: **s^**, all commands are case sensitive due to lazy coding by Unweal.)* \n stop - Stops spam. \n go - Allows spam again. \n help - ***BOIIIIIIII*** \n *Now get out of my head...* \n (Oh, and there's now s^stab, but who would want to do that to a cute girl like me?)");
 }
 if(message.content == prefix+"stab") {
+    console.log("Sayori has been killed in " + message.guild.name);
     message.reply("you left me hanging. \n \n        *wxth hxppy thxughts,* \n        Sayori");
     //dead = true;
     bot.user.setGame("with Yuri's pocket knife :')");
